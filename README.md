b1eaac01a5a7bfa7561b308ec433805996dc42b9
MAValle <mauvalle@gmail.com>
2019-06-03 19:00:14 -0400

    the last upgrade of today

---
+++ README.md
@@ -0,0 +1,22 @@
+## Genome-Wide-Ass_Data
+
+R codes and data for genome wide association study.
+
+
+
+
+
+Let me explain some files here:
+
+## Scripts
+
+- `script1R`: Testin a NN using neuralnet package on a test data base.
+
+- 
+
+  
+
+## Datasets and outputs
+
+- `bbdd_test030619csv.csv`: csv file sample from the original with 477 obs and 176 columns. 
+- 
--- script1.R
+++ script1.R
@@ -12,6 +12,9 @@
 # Notes:
 
 rm(list = ls())
+library(tidyverse)
+library(caret)
+library(neuralnet)
 # Lectura de datos
 data <- read.csv("bbdd_test030619csv.csv") # son 176 columnas.
 # Peligro que variable clase row 509 venia con un -9 (fue corregido en el excel)
@@ -37,7 +40,6 @@ f <- as.formula(paste("PHENOTYPE  ~", paste(n[1:length(n)], collapse = " + ") ) 
 
 
 # Using nural net
-library("neuralnet")
 model <- neuralnet(formula = f,
                    data= df,
                    hidden = 10,
@@ -57,12 +59,60 @@ preds <- predict(model, df[,c(7:176)], type="class")
 umbral=0.5
 preds2 <-  ifelse(preds > umbral, 1, 0)
 
-#boolean_integer <- function(arg1, umbral) {
-  arg1[arg1 >= umbral] <- 1
-  arg1[arg1 < umbral] <- 0
-  arg1 <- as.integer(arg1)
-}
-#preds2 <- boolean_integer(preds, umbral=0.5)
-
 #Calculate Classification accuracy
 table(preds2, df[,6])
+
+
+
+
+
+# # # # # # # # Utilizando 10 fold cross-validation
+# recurso: http://www.sthda.com/english/articles/38-regression-model-validation/157-cross-validation-essentials-in-r/
+
+set.seed(450)
+cv.error <- NULL
+k <- 10
+library(plyr) 
+pbar <- create_progress_bar('text')
+pbar$init(k)
+umbral = 0.5
+performance <- matrix(NA, ncol=5, nrow=10)
+for(i in 1:k){
+  index <- sample(1:nrow(df),round(0.9*nrow(df)))
+  train.cv <- df[index,]
+  test.cv <- df[-index,]
+  
+  # training the NN
+  nn <- neuralnet(formula = f, data= train.cv, hidden = 10, act.fct = "logistic", linear.output = FALSE,
+                  threshold = 0.01, lifesign = "minimal")
+  
+  
+  # Predict with the Test set
+  pr.nn <- predict(nn, test.cv[,c(7:176)], type="class")
+  # a binario
+  preds2 <-  ifelse(pr.nn  > umbral, 1, 0)
+  
+  #table(preds2, test.cv[,6])
+  cm <- as.matrix(table(Actual = test.cv[,6], Predicted = preds2)) # create the confusion matrix
+  # calculo de medidas de desempeno
+  # https://blog.revolutionanalytics.com/2016/03/com_class_eval_metrics_r.html
+  
+  n = sum(cm) # number of instances
+  nc = nrow(cm) # number of classes
+  diags = sum(diag(cm)) # number of correctly classified instances per class 
+  rowsums = apply(cm, 1, sum) # number of instances per class
+  colsums = apply(cm, 2, sum) # number of predictions per class
+  p = rowsums / n # distribution of instances over the actual classes
+  q = colsums / n # distribution of instances over the predicted classes
+  
+  acc = diags / n 
+  rec = cm[1,1]/(cm[1,1]+cm[1,2])
+  pre = cm[1,1]/(cm[1,1]+cm[2,1])
+  f1 = 2 * pre * rec / (pre + rec)
+  performance[i,] <- c(i, acc, pre, rec, f1)
+  
+  pbar$step()
+}
+colnames(performance) <- c("k", "acc", "pre", "rec", "f1")
+performance
+
